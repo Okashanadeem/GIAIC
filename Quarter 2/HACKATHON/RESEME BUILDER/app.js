cd083c9edc3f90312c89151a47d82dfc776df288
Okashanadeem <okashanadeem0101@gmail.com>
2024-09-08 20:48:40 +0500

    Update

--- Quarter 2/HACKATHON/RESEME BUILDER/app.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('toggle-skills').addEventListener('click', function () {
-    var skillsSection = document.getElementById('skills');
-    if (skillsSection.style.display === 'none') {
-        skillsSection.style.display = 'block';
-    }
-    else {
-        skillsSection.style.display = 'none';
-    }
-});
