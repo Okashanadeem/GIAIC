16c3c8598b64baee6dd1ece3da24442dc6ebea26
Okashanadeem <okashanadeem0101@gmail.com>
2024-09-14 00:30:24 +0500

    Update

---
+++ Quarter 2/class_01/my-app/app/layout.tsx
@@ -0,0 +1,11 @@
+import "./globals.css";
+
+export default function RootLayout({children}:any) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ Quarter 2/class_01/my-app/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello NextJs</h1>
+      <p>I am Okasha and this is my first Next.js project</p>
+    </div>
+  );
+}
